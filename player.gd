8e8b4844e5dd2fa19ded47d05d3914bc1cf43282
Lagswastaken <conkni25@kingshigh.school.nz>
2023-08-15 10:17:39 +1200

    V1

--- player.gd
+++ player.gd
@@ -9,26 +9,46 @@ signal health_changed(health_value)
 
 var health = 3
 
-const SPEED = 10.0
-const JUMP_VELOCITY = 10.0
+var objects_collected = 0
+
+@onready var ray =$Camera3D/RayCast3D
+@onready var interaction_notify = $Control/InteractionNotify
+@onready var collection_tracker = $Control/MarginContainer/CollectionTracker
+
+const SPEED = 5.0
+const JUMP_VELOCITY = 6.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = 20.0
+var gravity = 60.0
+
+func check_ray_hit():
+	if ray.is_colliding():
+		if ray.get_collider().is_in_group("Pickup"):
+			interaction_notify.visible = true
+		if Input.is_action_just_pressed("Use"):
+			ray.get_collider().queue_free()
+			objects_collected += 1
+			collection_tracker.text = "Mysterious Objects: " + str(objects_collected) + " /8"
+	else:
+		interaction_notify.visible = false
 
 
 func _ready():
-	
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 	camera.current = true
+	
 
 func _unhandled_input(event):
 	if event is InputEventMouseMotion:
 		rotate_y(-event.relative.x * .005)
 		camera.rotate_x(-event.relative.y * .005)
 		camera.rotation.x = clamp(camera.rotation.x, -PI/2, PI/2)
+	elif Input.is_action_just_pressed("quit"):
+		get_tree().quit()
 	
 func _physics_process(delta):
 	if not is_multiplayer_authority(): return
+	check_ray_hit()
 	
 	# Add the gravity.
 	if not is_on_floor():
